66f5fa6059848f9aa78e7b476681a69e1cb2f0b8
Ysabel Chen <ysabelchen@gmail.com>
2020-07-05 14:33:55 -0700

    WIP: message log and new message controller

--- Pawple/Controllers/SignUpViewController.swift
+++ Pawple/Controllers/SignUpViewController.swift
@@ -14,6 +14,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+//TODO: go to edit profile view controller
     @IBAction func signupPressed(_ sender: UIButton) {
         if let email = emailTextField.text, let password = passwordTextField.text {
             if !email.isValidEmail() {
@@ -32,6 +33,47 @@ class SignUpViewController: UIViewController {
                     self.navigationController?.dismiss(animated: true)
                 }
             }
+            
+            //Saves in firebase storage, but needs name and profile image
+//            Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+//
+//                if error != nil {
+//                    print(error!)
+//                    return
+//                }
+//
+//                guard let uid = user?.user.uid else {
+//                    return
+//                }
+//
+//                //successfully authenticated user
+//                let imageName = UUID().uuidString
+//                let storageRef = Storage.storage().reference().child("profile_images").child("\(imageName).jpg")
+//
+//                if let profileImage = self.profileImageView.image, let uploadData = profileImage.jpegData(compressionQuality: 0.1) {
+//
+//                    storageRef.putData(uploadData, metadata: nil, completion: { (_, err) in
+//
+//                        if let error = error {
+//                            print(error)
+//                            return
+//                        }
+//
+//                        storageRef.downloadURL(completion: { (url, err) in
+//                            if let err = err {
+//                                print(err)
+//                                return
+//                            }
+//
+//                            guard let url = url else { return }
+//                            let values = ["name": name, "email": email, "profileImageUrl": url.absoluteString]
+//
+//                            self.registerUserIntoDatabaseWithUID(uid, values: values as [String : AnyObject])
+//                        })
+//
+//                    })
+//                }
+//            })
         }
     }
     

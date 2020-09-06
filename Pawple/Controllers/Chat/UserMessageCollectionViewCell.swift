043180f59ddf841f647d9a4340bef811695acca2
Ysabel Chen <ysabelchen@gmail.com>
2020-09-06 12:56:46 -0700

    message bubble dynamic width

--- Pawple/Controllers/Chat/UserMessageCollectionViewCell.swift
+++ Pawple/Controllers/Chat/UserMessageCollectionViewCell.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class UserMessageCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            self.textView.contentInset = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 0)
+        }
+    }
+    
     @IBOutlet weak var viewBubble: UIView! {
         didSet {
             //TODO: adjust to liking
-            self.viewBubble.layer.cornerRadius = 15
+            self.viewBubble.layer.cornerRadius = 16
         }
     }
     
